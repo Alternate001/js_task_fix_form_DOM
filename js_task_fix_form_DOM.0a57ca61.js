e597b925a4b16b4669bea56cad56a4d7c60e87c8
Alternate001 <artem.kushko@publizer.de>
2026-03-31 10:13:42 +0200

    make build

---
+++ js_task_fix_form_DOM.0a57ca61.js
@@ -0,0 +1,2 @@
+"use strict";var inputs=document.querySelectorAll("input");inputs.forEach(function(e){var t=document.createElement("label");t.classList.add("field-label"),t.setAttribute("for",e.id),e.placeholder=e.name[0].toLocaleUpperCase()+e.name.slice(1),e.parentElement.append(t),t.textContent=e.name.toLocaleUpperCase()});
+//# sourceMappingURL=js_task_fix_form_DOM.0a57ca61.js.map
